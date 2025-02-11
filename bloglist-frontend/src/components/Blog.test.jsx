fec439741f18b24a3e96b0ab6d403c2fbff216e1
Stephen Dorssers <sdorssers7@gmail.com>
2025-02-11 13:17:23 -0500

    exercise 5.15 - testing user click like button calls add like event handler

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
-import { queryByText, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
+import { cleanup } from '@testing-library/react'
 
 beforeEach(() => {
   const blog = {
@@ -36,4 +37,27 @@ test('renders likes and url when view button clicked', async () => {
     // blog details div should be displayed after button click
     const div = screen.getByTestId('1003')
     expect(div).not.toHaveStyle('display:none')
+})
+
+test('handleAddLike event handler is called twice when like button is clicked twice', async() => {
+    // need to create different render for this test, so cleanup before
+    cleanup()
+    const blog = {
+      title: 'testing isnt fun but is necessary',
+      author: 'Stephen D',
+      url: 'http://testing.com',
+    }
+    // creates mock function to check event handler is being called
+    const mockAddLike = vi.fn()
+    render (<Blog blog={blog} handleAddLike={mockAddLike}/>)
+
+    // simulate user click twice
+    const user = userEvent.setup()
+    const button = screen.getByText('like')
+    for (let i=0; i < 2; i++) {
+        await user.click(button)
+    }
+
+    // check mock event handler called twice
+    expect(mockAddLike.mock.calls).toHaveLength(2) 
 })
